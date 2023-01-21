10e2bd01c33ccc9c215564c910f09b92bc6fc117
Dukula <dutandrew78@gmail.com>
2023-01-21 13:26:14 +0300

    using the the define preprocessor directive

--- define.c
+++ define.c
@@ -7,10 +7,11 @@ Preprocessing replaces all macros with their representive values.
  * define NAME value
  *NAME is whatever name you want to call it. it is advised to use upper case letters / capital letters when naming your macros.  
  *define starts with a hash sign '#' to let the compiler know that you defining a macro
+ * NOTE preprocess directives do not end with a semi-colon they are NOT C programs/ they are not part of the C language so there is no need to end them with a semi-colon
  */
 
 #define NAME "Dut Kulang"
-
+#define AGE 22
 int main(void)
 {
     printf("Name: %s\n", NAME);

d2ddf4a75213d8450a70716918fba8a596209363
Dukula <dutandrew78@gmail.com>
2023-01-21 13:16:35 +0300

    using the the define preprocessor directive

--- define.c
+++ define.c
@@ -3,11 +3,15 @@
  * Preprocessing is a process that happens before right the compilation process starts.
 
 Preprocessing replaces all macros with their representive values. 
- *
+ * define is used to define a macro
+ * define NAME value
+ *NAME is whatever name you want to call  
  */
+
+#define NAME "Dut Kulang"
 
 int main(void)
 {
-    
+    printf("Name: %s\n", NAME);
     return 0;
 }

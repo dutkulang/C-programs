d1d9f58089d8cb5ba2998dcf177c7f0f98002fa6
Dukula <dutandrew78@gmail.com>
2023-01-21 13:11:57 +0300

    using the the define preprocessor directive

---
+++ define.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Preprocessing is a process that happens before right the compilation process starts.
+
+Preprocessing replaces all macros with their representive values. 
+ *
+ */
+
+int main(void)
+{
+    
+    return 0;
+}

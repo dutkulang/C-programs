34faa771c6dad56a01b96aef556956102293a60b
Dut Kulang <dutandrew78@gmail.com>
2023-02-18 14:48:26 +0300

    rename file

--- add_one.c
+++ add_one.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int plus(int x)
-{
-    return (x+1);
-
-}
+int plus(int);
 
 int main(void)
 {
     int num1 = 11;
     printf("%d\n", plus(num1));
     return (0);
+}
+
+int plus(int x)
+{
+  // adds one to argument
+    return (x+1);
+
 }
